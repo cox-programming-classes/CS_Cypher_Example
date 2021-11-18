36b50600c1e474d95df701dd1c0d33cdc792b4b0
Jason Cox <coxj@vt.edu>
2021-11-18 12:28:19 -0500

    initial commit

---
+++ CS_Cypher_Example/CharSet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CS_Cypher_Example
+{
+    public partial class CharSet
+    {
+        /// <summary>
+        /// The Characters that can be used in a text cipher
+        /// </summary>
+        private static readonly string CHARSET =
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890,.?!@#$%^&*()_+=- \t\r\n";
+
+
+        /// <summary>
+        /// How many characters are in this CHARSET?
+        /// Useful for Modulo Arthmetic!
+        /// e.g.  int n = (Characters[ch] + 7) % Characters.Size
+        /// </summary>
+        public int Size => CHARSET.Length;
+        
+        /// <summary>
+        /// Get the Index of a character in the CHARSET
+        /// If the given character does not appear in the CHARSET, returns -1
+        /// </summary>
+        /// <param name="c">Character to search for</param>
+        public int this[char c] => CHARSET.ToCharArray().Contains(c) ? CHARSET.IndexOf(c) : -1;
+        
+        /// <summary>
+        /// Get the Character at position n in the CHARSET
+        /// if n is not valid, returns default (possibly null).
+        /// </summary>
+        /// <param name="n">Character Number to get back</param>
+        public char this[int n] => n >= 0 && n < CHARSET.Length ? CHARSET[n] : default;
+    }
+}
---
+++ CS_Cypher_Example/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CS_Cypher_Example
+{
+    internal class Program
+    {
+        private static CharSet Characters = new CharSet();
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Message:");
+            string message = Console.ReadLine();
+            
+            Console.WriteLine("Here are the values!");
+            foreach(char ch in message)
+                Console.Write($"{Characters[ch]} ");
+
+            Console.WriteLine();
+            
+            string encryptedMessage = CeaserCipherEncrypt(message, 17);
+            Console.WriteLine($"Encrypted Text:  {encryptedMessage}");
+
+            string decryptedMessage = CeaserCipherDecrypt(encryptedMessage, 17);
+            Console.WriteLine($"Decrypted Encrypted Text: {decryptedMessage}");
+        }
+
+        /// <summary>
+        /// Encrypt a message using a particular key for the Ceaser Cipher
+        /// </summary>
+        /// <param name="message">Message to Encrypt</param>
+        /// <param name="key">Secret Number used in the Encryption step</param>
+        /// <returns>Encrypted String</returns>
+        public static string CeaserCipherEncrypt(string message, int key)
+        {
+            string output = "";
+            foreach (char ch in message)
+            {
+                // try to look up the character number in the set.
+                int characterNumber = Characters[ch];
+                if (characterNumber != -1)
+                {
+                    characterNumber = (characterNumber + key) % Characters.Size;
+                    output += Characters[characterNumber];
+                }
+                else
+                {
+                    output += ch;
+                }
+            }
+
+            return output;
+        }
+        /// <summary>
+        /// Decrypt a message using a particular key for the Ceaser Cipher
+        /// </summary>
+        /// <param name="encryptedMessage">Message to Decrypt</param>
+        /// <param name="key">Secret Number used in the Encryption step</param>
+        /// <returns>Encrypted String</returns>
+        public static string CeaserCipherDecrypt(string encryptedMessage, int key)
+        {
+            string output = "";
+            foreach (char ch in encryptedMessage)
+            {
+                // try to look up the character number in the set.
+                int characterNumber = Characters[ch];
+                if (characterNumber != -1)
+                {
+                    characterNumber = (characterNumber - key) % Characters.Size;
+                    output += Characters[characterNumber];
+                }
+                else
+                {
+                    output += ch;
+                }
+            }
+
+            return output;
+        }
+    }
+}
